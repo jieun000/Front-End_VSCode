53458c9cbeed526461bfa3ef3455c037e8003ba8
jieun <mondieu0@naver.com>
2023-07-07 23:44:49 +0900

    VSCode1.0

--- 1/2_JS3/4_오픈웨덜.js
+++ 1/2_JS3/4_오픈웨덜.js
@@ -1,12 +1,12 @@
-const API_KEY = 'openweathermap API_KEY'; //https://openweathermap.org/current
-let name =document.querySelector('#name');
-let main =document.querySelector('#main');
-let temp =document.querySelector('#temp');
+const API_KEY = '8a6a1e856e8f61ecbf66ac1e4f5a529b'; //https://openweathermap.org/current
+let name = document.querySelector('#name');
+let main = document.querySelector('#main');
+let temp = document.querySelector('#temp');
 function connect(position) {
     // alert('위치연결 성공');
-    // console.log(position); //위도경도확인
-    // console.log(position.coords.latitude);                                                                                                              
-    // console.log(position.coords.longitude);
+    // console.log(position); // 위도&경도 확인
+    // console.log(position.coords.latitude);   // 위도
+    // console.log(position.coords.longitude);  // 경도
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
 
@@ -30,5 +30,3 @@ function errorConn() {
 }
 // navigator.geolocation.getCurrentPosition(함수, 함수);
 navigator.geolocation.getCurrentPosition(connect, errorConn);
-
-
